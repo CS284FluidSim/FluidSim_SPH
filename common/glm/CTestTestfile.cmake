e7a810b2e7fd1983059beb2b161433859040fcc8
lucas95123 <lucas95123@outlook.com>
2018-04-29 21:58:28 -0700

    added glm

--- common/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/VisualStudioProjects/glm-0.9.8.5/glm
-# Build directory: F:/VisualStudioProjects/glm-0.9.8.5/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
